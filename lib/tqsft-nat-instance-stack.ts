2c43fc42c824d456c40750135d020da1db2a7dd6
Raul Ramirez <raul.ramtap@gmail.com>
2024-05-29 20:58:39 -0400

    Nat Instance.

--- lib/tqsft-nat-instance-stack.ts
+++ lib/tqsft-nat-instance-stack.ts
@@ -1,16 +1,102 @@
 import * as cdk from 'aws-cdk-lib';
+import { AutoScalingGroup } from 'aws-cdk-lib/aws-autoscaling';
+import { InstanceClass, InstanceSize, InstanceType, KeyPair, LaunchTemplate, MachineImage, MultipartBody, MultipartUserData, OperatingSystemType, Peer, Port, SecurityGroup, SubnetType, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { Effect, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
+import { readFileSync } from 'fs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TqsftNatInstanceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpcCidr = cdk.Fn.importValue('Tqsft-VpcCidr');
+    const isolatedRouteTables = cdk.Fn.importValue('Tqsft-IsolatedRouteTables');
+    const keyPairName = new cdk.CfnParameter(this, 'KeyPairName', {
+      type: "String",
+      description: "Key Pair Name for SSH Access",
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TqsftNatInstanceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    })
+
+    const vpcId = StringParameter.valueFromLookup(this, 'TqsftStack-VpcId');
+    const vpc = Vpc.fromLookup(this, "vpc", {
+      vpcId: vpcId
+    });
+
+    const cloudConfig = UserData.custom(readFileSync('src/cloud-config.txt','utf8'));
+    const alterNAT = UserData.custom(readFileSync('src/alterNAT.sh', 'utf8').replace('${ROUTE_TABLES_IDS}',isolatedRouteTables));
+    
+    const multipartUserData = new MultipartUserData();
+    multipartUserData.addPart(MultipartBody.fromUserData(cloudConfig, "text/cloud-config"));
+    multipartUserData.addPart(MultipartBody.fromUserData(alterNAT, "text/x-shellscript"));
+
+    const instanceRole = new Role(this, 'MyRole', {
+      assumedBy: new ServicePrincipal('ec2.amazonaws.com'),
+      roleName: "NatInstanceProfile"
+    });
+    instanceRole.addManagedPolicy({
+      managedPolicyArn: "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+    })
+    instanceRole.addToPolicy(new PolicyStatement({
+      sid: 'alterNATInstancePermissions',
+      effect: Effect.ALLOW,
+      actions: [
+        'ec2:ModifyInstanceAttribute'
+      ],
+      resources: [ '*' ]
+    }));
+    instanceRole.addToPolicy(new PolicyStatement({
+      sid: 'alterNATInstanceRoute',
+      effect: Effect.ALLOW,
+      actions: [
+        'ec2:DescribeRouteTables',
+        'ec2:CreateRoute',
+        'ec2:ReplaceRoute'
+      ],
+      resources: [ '*' ]
+    }))
+
+    const launchTemplateSG = new SecurityGroup(this, "LaunchTemplateSG", {
+      vpc: vpc,
+      securityGroupName: "LaunchTemplateSG"
+    });
+
+    launchTemplateSG.addIngressRule(
+      Peer.ipv4(vpcCidr), 
+      Port.allTraffic(), 
+      "Ingress All Trafic in the subnet"
+    )
+
+    const keyPair = KeyPair.fromKeyPairName(this, "RaulRTKeyPair", keyPairName.valueAsString);
+
+    const launchTemplate = new LaunchTemplate(this, "LaunchTemplate", {
+      // requireImdsv2: true,
+      role: instanceRole,
+      instanceType: InstanceType.of(InstanceClass.T4G, InstanceSize.NANO),
+      machineImage: MachineImage.fromSsmParameter(
+          "/aws/service/canonical/ubuntu/server-minimal/22.04/stable/current/arm64/hvm/ebs-gp2/ami-id", {
+            os: OperatingSystemType.LINUX,
+            userData: multipartUserData
+        }
+      ),
+      keyPair: keyPair,
+      launchTemplateName: "NATInstancesLaunchTemplate",
+      securityGroup: launchTemplateSG,
+      
+    });
+
+    const natInstancesASG = new AutoScalingGroup(this, `nat-instances-asg`, {
+      vpc: vpc,
+      launchTemplate: launchTemplate,
+      minCapacity: 0,
+      maxCapacity: 1,
+      vpcSubnets: {
+        subnetType: SubnetType.PUBLIC
+      },
+      autoScalingGroupName: 'NatInstancesASG'
+    });
+
   }
 }
